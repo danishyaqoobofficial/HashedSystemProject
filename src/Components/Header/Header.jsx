1fcc4d956d0795b1bd6aec5544b5e8a5b1ac3312
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-06-03 13:35:24 +0500

    blog page completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -163,13 +163,13 @@ function Header() {
                                     <div className="rounded-[30px] bg-[#0D99FF] h-[3px] w-[0px] group-hover:w-full transition-all "></div>
                                 </div>
                             </Link>
-                            <Link to={`/`}>
+                            <Link to={`/portfolio`}>
                                 <div className=" capitalize text-[13px] lg:text-[14px] text-[#0D0A31] cursor-pointer font-[500] group Arimo ">
                                 portfolio
                                     <div className="rounded-[30px] bg-[#0D99FF] h-[3px] w-[0px] group-hover:w-full transition-all "></div>
                                 </div>
                             </Link>
-                            <Link to={`/`}>
+                            <Link to={`/blogs`}>
                                 <div className=" capitalize text-[13px] lg:text-[14px] text-[#0D0A31] cursor-pointer font-[500] group Arimo ">
                                 Blogs
                                     <div className="rounded-[30px] bg-[#0D99FF] h-[3px] w-[0px] group-hover:w-full transition-all "></div>
